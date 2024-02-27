7c427ccf4fa228e8f5daef242c5dce6e24569ffe
josedallatorre <jose.dallatorre@hotmail.com>
2024-02-27 20:45:51 +0100

    feat: modifying graph call

--- fetch-transcript/graph.py
+++ fetch-transcript/graph.py
@@ -19,23 +19,23 @@ from msgraph.generated.models.email_address import EmailAddress
 
 class Graph:
     settings: SectionProxy
-    device_code_credential: DeviceCodeCredential
-    user_client: GraphServiceClient
+    client_credential: DeviceCodeCredential
+    app_client: GraphServiceClient
 
     def __init__(self, config: SectionProxy):
         self.settings = config
         client_id = self.settings['clientId']
         tenant_id = self.settings['tenantId']
         graph_scopes = self.settings['graphUserScopes'].split(' ')
+        client_secret = self.settings['clientSecret']
 
-        self.device_code_credential = DeviceCodeCredential(client_id, tenant_id = tenant_id)
-        self.user_client = GraphServiceClient(self.device_code_credential, graph_scopes)
+        self.client_credential = DeviceCodeCredential(client_id=client_id, tenant_id= tenant_id)
+        self.app_client = GraphServiceClient(self.client_credential, scopes=graph_scopes) # type: ignore
 # </UserAuthConfigSnippet>
 
     # <GetUserTokenSnippet>
-    async def get_user_token(self):
-        graph_scopes = self.settings['graphUserScopes']
-        access_token = self.device_code_credential.get_token(graph_scopes)
+    async def get_app_only_token(self):
+        access_token = await self.client_credential.get_token(self.graph_scopes)
         return access_token.token
     # </GetUserTokenSnippet>
 
@@ -50,7 +50,7 @@ class Graph:
             query_parameters=query_params
         )
 
-        user = await self.user_client.me.get(request_configuration=request_config)
+        user = await self.app_client.me.get(request_configuration=request_config)
         return user
     # </GetUserSnippet>
 
@@ -68,7 +68,7 @@ class Graph:
             query_parameters= query_params
         )
 
-        messages = await self.user_client.me.mail_folders.by_mail_folder_id('inbox').messages.get(
+        messages = await self.app_client.me.mail_folders.by_mail_folder_id('inbox').messages.get(
                 request_configuration=request_config)
         return messages
     # </GetInboxSnippet>
@@ -91,13 +91,13 @@ class Graph:
         request_body = SendMailPostRequestBody()
         request_body.message = message
 
-        await self.user_client.me.send_mail.post(body=request_body)
+        await self.app_client.me.send_mail.post(body=request_body)
     # </SendMailSnippet>
 
     # <MakeGraphCallSnippet>
     async def make_graph_call(self):
         # INSERT YOUR CODE HERE
-        me = await self.user_client.me.get()
+        me = await self.app_client.me.get()
         if me:
             print(me.display_name)
 
@@ -112,11 +112,27 @@ class Graph:
         )
         """
 
-        result = await self.user_client.me.joined_teams.get()
+        result = await self.app_client.me.joined_teams.get()
         print(result)
+        """
         choice = input("id?")
-        result2 = await self.user_client.groups.by_group_id(choice).events.get()
+        result2 = await self.app_client.groups.by_group_id(choice).events.get()
         print(result2)
+        """
+        result3 = await self.app_client.communications.online_meetings.get()
+        print(result3)
+        # THE PYTHON SDK IS IN PREVIEW. FOR NON-PRODUCTION USE ONLY
+
+        result4 = await self.app_client.users.by_user_id('4c675eae-8304-4358-b9f7-d4480cfc798b').online_meetings.by_online_meeting_id('AAMkAGFjNmZhMDRiLTU1OGEtNGM3OC05MGFlLTllODk0OWYzNjFmYQBGAAAAAAD3p0rX5oaDSp0zUxLz21soBwBHgbmfciJ1Q6vNMjBkuvckAAAAAAENAABHgbmfciJ1Q6vNMjBkuvckAABrzcrQAAA=').transcripts.get()
+        print(result4)
+
+        #get children of coppola folder
+        #https://graph.microsoft.com/v1.0/groups/1fd60a75-9f61-437c-b4c5-5b400cbf9d4f/drive/items/01GQ6WVV7DNQJHIKYC4BFLBZOKORXABDCM/children/
+
+        #get a content
+        #https://graph.microsoft.com/v1.0/groups/1fd60a75-9f61-437c-b4c5-5b400cbf9d4f/drive/items/01GQ6WVVZS27LN2OKLGVBLQXK5ESUXJGK4/content
+
+
 
 
         return 
