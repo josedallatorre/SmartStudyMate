17c5f20d7de1c23a986188cb3f596fc39207fd6b
josedallatorre <jose.dallatorre@hotmail.com>
2024-05-06 23:44:39 +0200

    refactor with diynamic input

--- server/prova.py
+++ server/prova.py
@@ -1,26 +1,33 @@
 from azure.identity import InteractiveBrowserCredential
 from msgraph import GraphServiceClient
 import asyncio 
+import os 
 
 async def me():
+
     credential = InteractiveBrowserCredential()
     scopes = ['https://graph.microsoft.com/.default']
     graph_client = GraphServiceClient(credential, scopes)
     me = await graph_client.me.get()
     teams = await graph_client.me.joined_teams.get()
     print(teams)
-    group_id = input("group id?")
+    group_id = input("\n group id? \n")
     drive = await graph_client.groups.by_group_id(group_id).drive.get()
     print(drive)
-    drive_id = input("drive id?")
+    drive_id = input("\ndrive id? \n")
     root = await graph_client.drives.by_drive_id(drive_id).root.get()
     print(root)
-    childrens = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id('01GQ6WVV56Y2GOVW7725BZO354PWSELRRZ').children.get()
+    drive_item_id= input("\n drive item id? \n")
+    childrens = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(drive_item_id).children.get()
     print(childrens)
-    for child in childrens:
-        print(child["id"])
+    content_id = input("\n content id?")
+    content = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(content_id).content.get()
+    content = await graph_client.drives.by_drive_id('drive-id').items.by_drive_item_id('driveItem-id').content.get()
 
-    #get root folder of the group and list children
+    dir_path = os.path.dirname(os.path.realpath(content))
+    print(dir_path)
+
+    
     #https://graph.microsoft.com/v1.0/groups/1fd60a75-9f61-437c-b4c5-5b400cbf9d4f/drive/root/children
     
     #get children of "COPPOLA" folder
