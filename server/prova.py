a86537587d30834cde535a330a44da13d4613857
josedallatorre <jose.dallatorre@hotmail.com>
2024-05-09 15:03:44 +0200

    feat: now download file works

--- server/prova.py
+++ server/prova.py
@@ -1,11 +1,12 @@
 from azure.identity import InteractiveBrowserCredential
 from msgraph import GraphServiceClient
 from msgraph.generated.drives.item.items.item.children.children_request_builder import ChildrenRequestBuilder
-import asyncio 
-import os 
-from urllib.request import urlretrieve
-from os import open
 
+from msgraph.generated.drives.item.items.item.drive_item_item_request_builder import DriveItemItemRequestBuilder
+import asyncio 
+from urllib.request import urlretrieve
+from builtins import open
+import requests
 
 async def me():
 
@@ -35,19 +36,35 @@ async def me():
 
     childrens = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(drive_item_id).children.get(request_config)
     print(childrens)
-    #content = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(content_id).content.get()
-    #print("\n content:"+str(content))
-    """
-    urlretrieve(url_to_retrieve)
-    url = "https://databank.worldbank.org/data/download/WDI_CSV.zip"
-    response = requests.get(url, stream=True)
-    with requests.get(url, stream=True) as response:
-    # ...
-    with open("WDI_CSV.zip", mode="wb") as file:
-        for chunk in response.iter_content(chunk_size=10 * 1024):
-            file.write(chunk)
-    """
+    #content_id = '01GQ6WVV3KMPECR3QSTFAIRP7WNC6IMAWT'
+    content_id = '01GQ6WVV2O6XSJUPED4NH3QSCKWXJFFC7P'
+    
+    query_params = DriveItemItemRequestBuilder.DriveItemItemRequestBuilderGetQueryParameters(
+		select = ["id","@microsoft.graph.downloadUrl","name"],
+    )
 
+    request_configuration = DriveItemItemRequestBuilder.DriveItemItemRequestBuilderGetRequestConfiguration(
+    query_parameters = query_params,
+    )
+
+    content = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(content_id).get()
+    url_to_retrieve = str(content.additional_data.get('@microsoft.graph.downloadUrl'))
+
+    content = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(content_id).get()
+    print("\n content:\n"+str(content))
+    #content = await graph_client.drives.by_drive_id(drive_id).items.by_drive_item_id(content_id).content.get()
+
+
+
+    print(urlretrieve(url_to_retrieve, "file.mp4"))
+    
+    #url = "https://databank.worldbank.org/data/download/WDI_CSV.zip"
+    #response = requests.get(url_to_retrieve, stream=True)
+    #print(response.)
+    with requests.get(url_to_retrieve, stream=True) as response:
+        with open("file_request.mp4", "wb") as file:
+            for chunk in response.iter_content(chunk_size=10 * 1024):
+                file.write(chunk)
 
     
     #https://graph.microsoft.com/v1.0/groups/1fd60a75-9f61-437c-b4c5-5b400cbf9d4f/drive/root/children
