d1b9b4a372bb69207967740b71bbf320b3fe1de3
josedallatorre <jose.dallatorre@hotmail.com>
2024-05-21 23:39:19 +0200

    feat: config app to works with new schema

--- server/app_config.py
+++ server/app_config.py
@@ -3,30 +3,52 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-CLIENT_ID = "0c91214b-ac91-4e05-a828-ded6e4f77c73" # Application (client) ID of app registration
+if (os.getenv('B2C_TENANT_NAME')
+    and os.getenv('SIGNUPSIGNIN_USER_FLOW') and os.getenv('EDITPROFILE_USER_FLOW')):
+    # This branch is for B2C. You can delete this branch if you are not using B2C.
+    b2c_tenant = os.getenv('B2C_TENANT_NAME')
+    authority_template = "https://{tenant}.b2clogin.com/{tenant}.onmicrosoft.com/{user_flow}"
+    AUTHORITY = authority_template.format(
+        tenant=b2c_tenant,
+        user_flow=os.getenv('SIGNUPSIGNIN_USER_FLOW'))
+    B2C_PROFILE_AUTHORITY = authority_template.format(
+        tenant=b2c_tenant,
+        user_flow=os.getenv('EDITPROFILE_USER_FLOW'))
+    B2C_RESET_PASSWORD_AUTHORITY = authority_template.format(
+        # If you are using the new "Recommended user flow"
+        # (https://docs.microsoft.com/en-us/azure/active-directory-b2c/user-flow-versions),
+        # you can remove the B2C_RESET_PASSWORD_AUTHORITY settings from this file.
+        tenant=b2c_tenant,
+        user_flow=os.getenv('RESETPASSWORD_USER_FLOW'))
+else:  # This branch is for AAD or CIAM
+    # You can configure your authority via environment variable
+    # Defaults to a multi-tenant app in world-wide cloud
+    AUTHORITY = os.getenv("AUTHORITY") or "https://login.microsoftonline.com/common"
 
-# In a production app, we recommend you use a more secure method of storing your secret,
-# like Azure Key Vault. Or, use an environment variable as described in Flask's documentation:
-# https://flask.palletsprojects.com/en/1.1.x/config/#configuring-from-environment-variables
+# Application (client) ID of app registration
+CLIENT_ID = os.getenv("CLIENT_ID")
+# Application's generated client secret: never check this into source control!
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-if not CLIENT_SECRET:
-    raise ValueError("Need to define CLIENT_SECRET environment variable")
-
-AUTHORITY = "https://login.microsoftonline.com/organizations"  # For multi-tenant app
-# AUTHORITY = "https://login.microsoftonline.com/Enter_the_Tenant_Name_Here"
 
 REDIRECT_PATH = "/getAToken"  # Used for forming an absolute URL to your redirect URI.
-                              # The absolute URL must match the redirect URI you set
-                              # in the app's registration in the Azure portal.
+# The absolute URL must match the redirect URI you set
+# in the app's registration in the Azure portal.
 
 # You can find more Microsoft Graph API endpoints from Graph Explorer
 # https://developer.microsoft.com/en-us/graph/graph-explorer
-#ENDPOINT2 = 'https://graph.microsoft.com/v1.0/users/4c675eae-8304-4358-b9f7-d4480cfc798b/onlineMeetings/AAMkAGFjNmZhMDRiLTU1OGEtNGM3OC05MGFlLTllODk0OWYzNjFmYQBGAAAAAAD3p0rX5oaDSp0zUxLz21soBwBHgbmfciJ1Q6vNMjBkuvckAAAAAAENAABHgbmfciJ1Q6vNMjBkuvckAABrzcrQAAA=/transcripts'  # This resource requires no admin consent
-ENDPOINT2 = 'https://graph.microsoft.com/v1.0/me/joinedTeams'
-ENDPOINT = 'https://graph.microsoft.com/v1.0/me/'
+ENDPOINT = 'https://graph.microsoft.com/v1.0/me'  # This resource requires no admin consent
 
 # You can find the proper permission names from this document
 # https://docs.microsoft.com/en-us/graph/permissions-reference
-SCOPE = "https://graph.microsoft.com/.default"
+SCOPE = ["User.Read","Team.ReadBasic.All"]
 
-SESSION_TYPE = "filesystem"  # Specifies the token cache should be stored in server-side session
+# Tells the Flask-session extension to store sessions in the filesystem
+SESSION_TYPE = "filesystem"
+# In production, your setup may use multiple web servers behind a load balancer,
+# and the subsequent requests may not be routed to the same web server.
+# In that case, you may either use a centralized database-backed session store,
+# or configure your load balancer to route subsequent requests to the same web server
+# by using sticky sessions also known as affinity cookie.
+# [1] https://www.imperva.com/learn/availability/sticky-session-persistence-and-cookies/
+# [2] https://azure.github.io/AppService/2016/05/16/Disable-Session-affinity-cookie-(ARR-cookie)-for-Azure-web-apps.html
+# [3] https://learn.microsoft.com/en-us/azure/app-service/configure-common?tabs=portal#configure-general-settings
