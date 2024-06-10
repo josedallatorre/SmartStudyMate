a97e6768caaf25cb37cc7787334f2ee3151b6b03
josedallatorre <jose.dallatorre@hotmail.com>
2024-06-10 22:53:06 +0200

    fix: added proxy

--- flask/wsgi.py
+++ flask/wsgi.py
@@ -1,5 +1,10 @@
 from app import app
 import os
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 
 if __name__ == "__main__":
+    app.wsgi_app = ProxyFix(
+    app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1
+    )
     app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"), debug=True)
