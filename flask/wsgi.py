1ad5d9c7a0d799c86b26d09697ffce7e912f07ba
josedallatorre <jose.dallatorre@hotmail.com>
2024-06-13 16:19:09 +0200

    fix: not used anymore

--- flask/wsgi.py
+++
@@ -1,7 +0,0 @@
-from app import app
-import os
-from werkzeug.middleware.proxy_fix import ProxyFix
-
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=os.environ.get("FLASK_SERVER_PORT"), debug=True)
