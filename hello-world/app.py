eb24d21e98f7ac79edcd38f819caa1e4d89b410a
josedallatorre <jose.dallatorre@hotmail.com>
2024-07-13 17:35:20 +0200

    feat: now recieving json file in gpu

--- hello-world/app.py
+++ hello-world/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
-
+from flask import Flask,  request
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route("/handle_data", methods=['POST'])
+def handle_data():
+    selected_contents = request.get_json()
+    print("ciao",selected_contents)
+    return selected_contents
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
