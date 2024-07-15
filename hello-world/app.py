ad315a4b81e8f6ef9595a3634413360241ba8571
josedallatorre <jose.dallatorre@hotmail.com>
2024-07-15 09:58:16 +0200

    feat: moving download to gpu

--- hello-world/app.py
+++ hello-world/app.py
@@ -1,16 +1,71 @@
+import ast
+import asyncio
+import json
+import os
+import threading
+import time
+import aiohttp
 from flask import Flask,  request
 app = Flask(__name__)
+
+# Store for generated files and their progress
+download_progress = {}
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route("/handle_data", methods=['POST'])
-def handle_data():
+@app.route("/handle_data/<file_id>", methods=['POST'])
+def handle_data(file_id):
     selected_contents = request.get_json()
-    print("ciao",selected_contents)
+    my_list = [ast.literal_eval(item) for item in selected_contents]
+    print("ciao",selected_contents,type(selected_contents))
+    print("ciao12",my_list,type(my_list))
     file_id = str(time.time())  # Simple unique ID for the download session
+    for content in my_list:
+        print("ciao122",content,type(content))
+        threading.Thread(target=start_download, args=(file_id,content,)).start()
     return selected_contents
+
+def start_download(file_id,content):
+    asyncio.run(download_file(content))
+
+async def download_file(content):
+    print(content)
+    filename = content['id'] + ".mp4"
+    #path = os.path.join('static',filename)
+    path = filename
+    # Check whether the specified file exists or not 
+    if(os.path.exists(path)):
+        print('file already exists, skippping: ',filename)
+        update_progress(content.id, 100)
+    else:
+        async with aiohttp.ClientSession() as session:
+            try:
+                async with session.get(content['@microsoft.graph.downloadUrl']) as response:
+                    if response.status != 200:
+                        resp ={"error": f"server returned {response.status}"}
+                    else:
+                        total_size = int(response.headers.get('Content-Length', 0))
+                        chunk_size = 1024 * 1024  # 1MB
+                        downloaded_size = 0
+                        print(filename)
+                        with open(path, mode="wb") as file:
+                            async for chunk in response.content.iter_chunked(chunk_size):
+                                if chunk:
+                                    file.write(chunk)
+                                    downloaded_size += len(chunk)
+                                    progress = (downloaded_size / total_size) * 100
+                                    update_progress(content.id, progress)
+                        update_progress(content.id, 100)
+                        print(f"Downloaded file {content['name']}")
+            except asyncio.TimeoutError:
+                print(f"timeout error on {content['@microsoft.graph.downloadUrl']}")
+
+def update_progress(content_id, progress):
+    if content_id in download_progress:
+        download_progress[content_id] = progress
+        print(f"Progress for file {content_id}: {progress}%")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
