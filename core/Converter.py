7fcd5c95c97c1402534b2483cd742773e203033d
Diletta <dilettagiabardo@gmail.com>
2024-09-15 15:04:42 +0200

    update converter

--- core/Converter.py
+++ core/Converter.py
@@ -11,7 +11,7 @@ class MyBarLogger(ProgressBarLogger):
     def bars_callback(self, bar, attr, value,old_value=None):     
         percentage = (value / self.bars[bar]['total']) * 100
         #Print the percentage of the convertion
-        print(percentage)
+        #print(percentage)
 
 logger = MyBarLogger()
 
@@ -19,7 +19,9 @@ logger = MyBarLogger()
 #pathVideo is a list of the Path of the videos
 #Converter from mp4 to mp3
 def useConverter(pathVideo, courseName, email) :
-    
+
+    print("Inizio conversione")
+
     pathList = []
 
     for path in pathVideo:
@@ -28,7 +30,7 @@ def useConverter(pathVideo, courseName, email) :
       if not os.path.exists("Mp3"):
         os.makedirs("Mp3")
 
-      pathMp3 = Path("Mp3") / (str(path).replace(".mp4", ".mp3"))
+      pathMp3 = Path("Mp3") / path.split('/')[-1].replace(".mp4", ".mp3")
 
       pathList = pathList + [pathMp3]
 
@@ -38,4 +40,5 @@ def useConverter(pathVideo, courseName, email) :
         video.audio.write_audiofile(pathMp3, logger=logger)
 
     #Call whisper for the transcription
+    print("Conversione finita")
     Whisper.main(pathList, courseName, email)
